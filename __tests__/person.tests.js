10694bcbfef9051e3d9244c44a088667117efc21
Justin Shaffer <justinbshaffer91@gmail.com>
2023-03-10 14:50:49 -0800

    create Person class, test PASS

--- __tests__/person.tests.js
+++ __tests__/person.tests.js
@@ -1,7 +1,17 @@
+const Person = require('../src/person.js');
+
 test('should fail', () => {
   expect(true).toBe(false);
 });
 
 test('should pass', () => {
   expect(true).toBe(true);
+});
+
+test('should create a Person object with the correct properties', () => {
+  const person = new Person('John', 'Doe', 30, 'male');
+  expect(person.firstName).toBe('John');
+  expect(person.lastName).toBe('Doe');
+  expect(person.age).toBe(30);
+  expect(person.gender).toBe('male');
 });
---
+++ src/person.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor(firstName, lastName, age, gender) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+module.exports = Person;
